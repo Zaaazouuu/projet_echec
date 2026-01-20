a8514b68c26ab3e29b49afbb6f00e4fdbd055fc9
Nathan Julien <nathan.julien@etu.minesparis.psl.eu>
2026-01-20 15:00:53 +0100

    pixelisation du damier et position initiale du jeu

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pyxel
 
-side_length =8
+side_length = 16
+case_per_line = 8
 
-damier = []
-for i in range (0, side_length):
-    for j in range (0,side_length): 
+
+damier =[]
+for i in range (0, case_per_line):
+    for j in range (0,case_per_line): 
         if i%2==j%2 : 
             damier.append((i,j))
 
@@ -12,7 +14,7 @@ for i in range (0, side_length):
 class App:
     def __init__(self,start):
         self.start=start
-        pyxel.init(side_length,side_length, title="Jeu d'échec")
+        pyxel.init(side_length*case_per_line,side_length*case_per_line, title="Jeu d'échec")
         pyxel.run(self.update, self.draw)
         
 
@@ -26,6 +28,8 @@ class App:
     
     def draw_maze(self):
         for x,y in self.start :
-            pyxel.pset(x,y,7)
+            for i in range (0,side_length) :
+                for j in range (0,side_length):
+                    pyxel.pset(side_length*x+i,y*side_length+j,7)
 
 App(damier)
--- position_data.py
+++ position_data.py
@@ -1,5 +1,21 @@
+from main import case_per_line
+
+pions_sépciaux_joueur1=["tour1","cavalier1","fou1","dame1","roi1","fou1","cavalier1","tour1"]
+pions_sépciaux_joueur2=["tour2","cavalier2","fou2","dame2","roi2","fou2","cavalier2","tour2"]
+
+def initialisation_position():
+    positions_initiale = {}
+    for j in range (2, case_per_line-2):
+        for i in range (0,case_per_line): 
+            positions_initiale[(i,j)]="None"
+    for i in range (0,case_per_line): 
+        positions_initiale[(i,1)]="pion1"
+        positions_initiale[(i,0)]=pions_sépciaux_joueur1[i]
+        positions_initiale[(i,case_per_line-1)]="pion2"
+        positions_initiale[(i,case_per_line)]=pions_sépciaux_joueur2[i]
+    return positions_initiale
+
+        
 
 
-positions_initiale = {}
-    
 
