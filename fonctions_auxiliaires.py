48e4dcc8b32cbbe9b1b7fa387acf728f014782c6
Nathan Julien <nathan.julien@etu.minesparis.psl.eu>
2026-01-21 16:32:52 +0100

    changement de couleur du damier quand le roi est 'mangé'

--- fonctions_auxiliaires.py
+++ fonctions_auxiliaires.py
@@ -79,9 +79,7 @@ def mvt_cheval(vecteur_deplacement):
 
 
 def mvt_tour(vecteur_deplacement):
-   
-    m=[(0, -7), (0, -6), (0, -5), (0, -4), (0, -3), (0, -2), (0, -1), (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (-7, 0), (-6, 0), (-5, 0), (-4, 0), (-3, 0), (-2, 0), (-1, 0), (0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)]
-    if vecteur_deplacement in (m):
+    if vecteur_deplacement in ([(0, n) for n in range(-7,8)] + [(n, 0) for n in range(-7,8)]):
         return True
     return False
 
