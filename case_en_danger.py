c32bd7b7354919b8ba6c50d1eb4fcfbf65c55648
Gabriel Baussant <gabriel.baussant@etu.minesparis.psl.eu>
2026-01-27 15:40:22 +0100

    modif case_en danger

--- case_en_danger.py
+++ case_en_danger.py
@@ -20,32 +20,51 @@ def detection_echec(position):
     cases_menacees_2 = set()
 
     for case, piece in pieces_j1:
-        if piece is not "pion1":
+        if piece != "pion1":
             for i in range(8):
                 for j in range(8):
                    copie_position = position.copy()
-                   nouvelle_position = gestion_commande(copie_position, case, (i, j),1)[1]
+                   nouvelle_position = gestion_commande(copie_position, case, (i, j),"1")[1]
                    if nouvelle_position != position:
                         cases_menacees_1.add((i, j))
         else : 
-            cases_menacees_1.add((case[0]-1,case[1]-1))
-            cases_menacees_1.add((case[0]+1,case[1]-1))
+            cases_menacees_1.add((case[0]-1,case[1]+1))
+            cases_menacees_1.add((case[0]+1,case[1]+1))
 
     for case, piece in pieces_j2:
-        if piece is not "pion2":
+        if piece != "pion2":
             for i in range(8):
                 for j in range(8):
                     copie_position = position.copy()
-                    nouvelle_position = gestion_commande(copie_position, case, (i, j),2)[1]
+                    nouvelle_position = gestion_commande(copie_position, case, (i, j),"2")[1]
                     if nouvelle_position != position:
                         cases_menacees_2.add((i, j))
         else :
-            cases_menacees_2.add((case[0]-1,case[1]+1))
-            cases_menacees_2.add((case[0]+1,case[1]+1))
+            cases_menacees_2.add((case[0]-1,case[1]-1))
+            cases_menacees_2.add((case[0]+1,case[1]-1))
     if position_roi1 in cases_menacees_2  :
-        return "roi2"
+        return "roi1"
     if position_roi2 in cases_menacees_1 : 
-        return "roi1" 
-    return None 
+        return "roi2"
+    return None
         
 
+
+case_per_line=8
+
+pions_spéciaux_joueur1=["tour1","cavalier1","fou1","dame1","roi1","fou1","cavalier1","tour1"]
+pions_spéciaux_joueur2=["tour2","cavalier2","fou2","dame2","roi2","fou2","cavalier2","tour2"]
+
+def initialisation_position():
+    positions_initiale = {}
+    for j in range (2, case_per_line-2):
+        for i in range (0,case_per_line): 
+            positions_initiale[(i,j)]=None
+    for i in range (0,case_per_line): 
+        positions_initiale[(i,1)]="pion1"
+        positions_initiale[(i,0)]=pions_spéciaux_joueur1[i]
+        positions_initiale[(i,case_per_line-2)]="pion2"
+        positions_initiale[(i,case_per_line-1)]=pions_spéciaux_joueur2[i]
+    return positions_initiale
+
+detection_echec(initialisation_position())
